bdb19c32510163067c6242d09f146dcdc27812cf
Harold Sumbrana <harbatdays@gmail.com>
2018-12-06 02:43:41 +0800

    packagejson add homepage property

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
 	<header class="row">
 		<nav class="navbar navbar-toggleable-md navbar-light">
-			<router-link to="/menu" tag="li"><<a class="navbar-brand">PIZZA PLANET</a></router-link>
+			<router-link to="/menu" tag="li"><<a class="navbar-brand">HAROLD"S PIZZA</a></router-link>
 			<ul class="navbar-nav">
 				
 					<router-link to="/" tag="li"><a class="nav-link">home</a></router-link>
