2eb853c59ab03c6103bac3e189e06faa354cbfd8
Harold Sumbrana <harbatdays@gmail.com>
2018-12-06 05:06:47 +0800

    adding about styles

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,3 +1,16 @@
 <template>
+<div id="aboutHead">
 	<h1>About Us</h1>
+</div>	
 </template>
+
+<style type="text/css">
+	
+	#aboutHead {
+    background: url('../../src/assets/pizza.jpg');
+    width: 100% !important;
+    background-size: 100%;
+    background-color: #ffffff80;
+
+	}
+</style>
