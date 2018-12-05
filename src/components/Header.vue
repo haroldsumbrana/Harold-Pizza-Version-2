73fd780447f778ee7cb747f44276868f86f2c146
Harold Sumbrana <harbatdays@gmail.com>
2018-12-06 02:10:50 +0800

    update 3 commiting

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,14 @@
 <template>
 	<header class="row">
 		<nav class="navbar navbar-toggleable-md navbar-light">
-			<router-link to="/"><a class="navbar-brand">HAROLD'S PIZZA</a></router-link>
+			<a href="/" class="navbar-brand">PIZZA PLANET</a>
 			<ul class="navbar-nav">
 				
 					<router-link to="/" tag="li"><a class="nav-link">home</a></router-link>
-			
-				
+					<router-link to="/about" tag="li"><a class="nav-link">about</a></router-link>
+					<router-link to="/admin" tag="li"><a class="nav-link">admin</a></router-link>
 					<router-link to="/menu" tag="li"><a class="nav-link">menu</a></router-link>
+
 			
 				
 				
