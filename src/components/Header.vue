e1dbaa52a91dd86b4630f8055b4bde666f12f2c0
Harold Sumbrana <harbatdays@gmail.com>
2018-12-06 02:50:20 +0800

    normal changes

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,10 @@
 <template>
 	<header class="row">
 		<nav class="navbar navbar-toggleable-md navbar-light">
-			<router-link to="/menu" tag="li"><<a class="navbar-brand">HAROLD"S PIZZA</a></router-link>
+
+
+
+			<router-link to="/menu" tag="li"><a class="navbar-brand">HAROLD"S PIZZA</a></router-link>
 			<ul class="navbar-nav">
 				
 					<router-link to="/" tag="li"><a class="nav-link">home</a></router-link>
@@ -16,5 +19,4 @@
 		</nav>
 
 	</header>
-
 </template>
